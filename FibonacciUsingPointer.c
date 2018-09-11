7c143a77d18761ffeb2a499780ba9c7853703198
sky6597 <40659414+sky6597@users.noreply.github.com>
2018-09-11 10:30:42 -0700

    Update FibonacciUsingPointer.c

--- FibonacciUsingPointer.c
+++ FibonacciUsingPointer.c
@@ -2,21 +2,21 @@
 #include<stdlib.h>
 int main()
 {
-int a,b,c,i,n;
-b=0;
-c=1;
-printf("enter the value \n");
-scanf("%d",&a);
-int *d;
-d=(int*)malloc(sizeof(a));
-*(d+0)=0;
-*(d+1)=1;
-i=2;
-while(i<a)
-{
-*(d+i)=b+c;b=c;c=*(d+i);
-i++;
-}
-for(i=0;i<a;i++)
-printf("%d\n",*(d+i));
+  int a,b,c,i,n;
+  b=0;
+  c=1;
+  printf("enter the value \n");
+  scanf("%d",&a);
+  int *d;
+  d=(int*)malloc(sizeof(a));
+  *(d+0)=0;
+  *(d+1)=1;
+  i=2;
+  while(i<a)
+  {
+    *(d+i)=b+c;b=c;c=*(d+i);
+    i++;
+  }
+  for(i=0;i<a;i++)
+  printf("%d\n",*(d+i));
 }
